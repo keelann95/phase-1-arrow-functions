1557081590bf2fdbae64564280a11a432b00b666
keelann95 <sephen.mwaniki@student.moringaschool.com>
2024-07-10 12:27:25 +0300

    modified  index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+// Function expression for divide
+let divide = function(a, b) {
+    return a / b;
+  }
+  
+  // Arrow function for square
+  let square = (x) => {
+    return x * x;
+  }
+  
+  // Arrow function for add
+  let add = (a, b) => {
+    return a + b;
+  }
+  
+  module.exports = {
+    divide,
+    square,
+    add
+  };
+  
